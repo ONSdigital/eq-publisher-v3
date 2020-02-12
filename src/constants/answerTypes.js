34ddaf5150b3995c740de82b79b71c407e4dc8d9
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-12 12:37:00 +0000

    Add constants

---
+++ src/constants/answerTypes.js
@@ -0,0 +1,25 @@
+const CHECKBOX = "Checkbox";
+const RADIO = "Radio";
+const TEXTFIELD = "TextField";
+const TEXTAREA = "TextArea";
+const CURRENCY = "Currency";
+const NUMBER = "Number";
+const DATE = "Date";
+const DATE_RANGE = "DateRange";
+const PERCENTAGE = "Percentage";
+const UNIT = "Unit";
+const DURATION = "Duration";
+
+module.exports = {
+  CHECKBOX,
+  RADIO,
+  TEXTFIELD,
+  TEXTAREA,
+  CURRENCY,
+  NUMBER,
+  DATE,
+  DATE_RANGE,
+  PERCENTAGE,
+  UNIT,
+  DURATION
+};
