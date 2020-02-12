85bb6c9e2c0c2186a80071478bbbacec586001f8
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-12 13:10:00 +0000

    Move index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const { SOCIAL } = require("./constants/questionnaireTypes");
+
+console.log(SOCIAL);
+
 const app = express();
 
 const status = require("./middleware/status");
@@ -6,6 +10,10 @@ const status = require("./middleware/status");
 const PORT = process.env.PORT || 9000;
 
 app.get("/status", status);
+
+app.get("/:questionnaireid", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Listening on port", PORT); // eslint-disable-line
