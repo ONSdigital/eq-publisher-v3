44bd6bfe1797e63cc184d27b4c84fa12c3140e88
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-13 12:08:40 +0000

    Rename folder and refactor convertSchema

--- src/middleware/schemaConverter/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = converter => (req, res, next) => {
-  converter
-    .convert(res.locals.questionnaire)
-    .then(questionnaire => {
-      res.locals.questionnaire = questionnaire;
-      next();
-    })
-    .catch(err => next(err));
-};
