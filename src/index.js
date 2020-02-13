0e72fa54946f5264d724339befd1bd5604e164d1
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-13 12:08:10 +0000

    Remove excess and restructure

--- src/index.js
+++ src/index.js
@@ -3,25 +3,12 @@ const app = express();
 
 const questionnaireJSON = require("../authorTest.json");
 
-const status = require("./middleware/status");
-const respondWithData = require("./middleware/respondWithData");
-const schemaConverter = require("./middleware/schemaConverter");
-
-const SchemaValidator = require("./validation/SchemaValidator");
-const ValidationApi = require("./validation/ValidationApi");
-
-const Convert = require("./Convert");
-
-const converter = new Convert(
-  new SchemaValidator(new ValidationApi(process.env.EQ_SCHEMA_VALIDATOR_URL))
-);
-
-const { get, set } = require("lodash");
-const fetchLocalData = json => async (req, res, next) => {
-  const questionnaire = get(json, "data.questionnaire");
-  set(res, "locals.questionnaire", questionnaire);
-  next();
-};
+const {
+  convertSchema,
+  fetchQuestionnaire,
+  respondWithData,
+  status
+} = require("./middleware");
 
 const PORT = process.env.PORT || 9000;
 
@@ -29,12 +16,9 @@ app.get("/status", status);
 
 app.get(
   "/testpoint",
-  fetchLocalData(questionnaireJSON),
-  schemaConverter(converter),
-  respondWithData,
-  (req, res) => {
-    res.send("hello");
-  }
+  fetchQuestionnaire(questionnaireJSON),
+  convertSchema,
+  respondWithData
 );
 
 app.listen(PORT, "0.0.0.0", () => {
