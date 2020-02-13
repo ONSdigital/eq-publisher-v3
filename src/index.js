dd6e92227c80f328e14c73459c5ede62b034db5e
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-13 09:04:11 +0000

    Add schemaValidator

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 const express = require("express");
-const { SOCIAL } = require("./constants/questionnaireTypes");
-
-console.log(SOCIAL);
-
 const app = express();
 
+const questionnaireJSON = require("../authorTest.json");
+
 const status = require("./middleware/status");
+const respondWithData = require("./middleware/respondWithData");
+const schemaConverter = require("./middleware/schemaConverter");
+
+const SchemaValidator = require("./validation/SchemaValidator");
+const ValidationApi = require("./validation/ValidationApi");
+
+const Convert = require("./Convert");
+
+const converter = new Convert(
+  new SchemaValidator(new ValidationApi(process.env.EQ_SCHEMA_VALIDATOR_URL))
+);
+
+/*
+  make a dummy fetch data here
+  pass in the json data
+  set the locals part of the res object to accept the questionnaire
+  .i.e res.locals.questionnaire = questionnaire
+  then return next()
+*/
+const { get, set } = require("lodash");
+const fetchLocalData = json => async (req, res, next) => {
+  const questionnaire = get(json, "data.questionnaire");
+  set(res, "locals.questionnaire", questionnaire);
+  next();
+};
 
 const PORT = process.env.PORT || 9000;
 
 app.get("/status", status);
 
-app.get("/:questionnaireid", (req, res) => {
-  res.send("hello");
-});
+app.get(
+  "/testpoint",
+  fetchLocalData(questionnaireJSON),
+  schemaConverter(converter),
+  respondWithData,
+  (req, res) => {
+    res.send("hello");
+  }
+);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Listening on port", PORT); // eslint-disable-line
