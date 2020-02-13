0e0e1bbf5bce9ff8854f3196d3b39fcea98ad8cf
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-13 12:08:57 +0000

    Add index file for middleware

---
+++ src/middleware/index.js
@@ -0,0 +1,11 @@
+const convertSchema = require("./convertSchema");
+const fetchQuestionnaire = require("./fetchQuestionnaire");
+const respondWithData = require("./respondWithData");
+const status = require("./status");
+
+module.exports = {
+  convertSchema,
+  fetchQuestionnaire,
+  respondWithData,
+  status
+};
