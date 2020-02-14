cef3d645ce71e1f43fdcdde4a8d59bd928f6c47f
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-14 11:18:12 +0000

    Add auth token

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,12 @@
 const convertSchema = require("./convertSchema");
+const createAuthToken = require("./createAuthToken");
 const fetchQuestionnaire = require("./fetchQuestionnaire");
 const respondWithData = require("./respondWithData");
 const status = require("./status");
 
 module.exports = {
   convertSchema,
+  createAuthToken,
   fetchQuestionnaire,
   respondWithData,
   status
