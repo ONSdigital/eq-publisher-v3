823bb2f3c280bfe2be8fe9853cb3043f9ef048fc
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-17 09:37:34 +0000

    Add validation

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,11 +3,13 @@ const createAuthToken = require("./createAuthToken");
 const fetchQuestionnaire = require("./fetchQuestionnaire");
 const respondWithData = require("./respondWithData");
 const status = require("./status");
+const validation = require("./validation");
 
 module.exports = {
   convertSchema,
   createAuthToken,
   fetchQuestionnaire,
   respondWithData,
-  status
+  status,
+  validation
 };
