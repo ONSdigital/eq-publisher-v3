05c9c439974780a5dff6576e8d92ba9ad78c33b7
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-19 08:51:29 +0000

    Comment out validation until needed

--- src/middleware/validation/index.js
+++ src/middleware/validation/index.js
@@ -1,14 +1,12 @@
-const SchemaValidator = require("./SchemaValidator");
-const ApiValidator = require("./ValidationApi");
+// const SchemaValidator = require("./SchemaValidator");
+// const ApiValidator = require("./ValidationApi");
 
 const validation = async (req, res, next) => {
-  const api = new ApiValidator(process.env.EQ_SCHEMA_VALIDATOR_URL);
-  const validator = new SchemaValidator(api);
-  const result = await validator.validate(res.locals.questionnaire);
-  console.log("here");
-  if (!result.valid) {
-    console.log("cdd");
-  }
+  // const api = new ApiValidator(process.env.EQ_SCHEMA_VALIDATOR_URL);
+  // const validator = new SchemaValidator(api);
+  // const result = await validator.validate(res.locals.questionnaire);
+  // if (!result.valid) {
+  // }
   next();
 };
 
