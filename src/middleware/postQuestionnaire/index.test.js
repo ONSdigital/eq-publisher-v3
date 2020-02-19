bdeeac4e4bcf506cb1d84df388bf46a23af543b0
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-19 08:52:06 +0000

    Add middleware test

---
+++ src/middleware/postQuestionnaire/index.test.js
@@ -0,0 +1,35 @@
+const postQuestionnaire = require(".");
+
+describe("PostQuestionnaire", () => {
+  let req, res, next, questionnaire;
+  let middleware; // eslint-disable-line
+  beforeEach(() => {
+    questionnaire = {
+      id: "123",
+      title: "Survey"
+    };
+    req = {
+      body: {}
+    };
+    res = {
+      sendStatus: jest.fn(),
+      locals: {}
+    };
+    next = jest.fn();
+  });
+
+  it("should return an error with invalid JSON", () => {
+    middleware = postQuestionnaire(req, res, next);
+    expect(res.sendStatus).toHaveBeenCalled();
+  });
+
+  it("should move to next if successful", () => {
+    req.body.data = {};
+    req.body.data.questionnaire = questionnaire;
+
+    middleware = postQuestionnaire(req, res, next);
+
+    expect(res.locals.questionnaire).toEqual(questionnaire);
+    expect(next).toHaveBeenCalled();
+  });
+});
