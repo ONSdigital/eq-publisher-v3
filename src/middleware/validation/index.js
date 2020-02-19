4dd8f4aa8ed842a01359232619c3fc329af80b4c
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-19 15:49:41 +0000

    Update validator URL

--- src/middleware/validation/index.js
+++ src/middleware/validation/index.js
@@ -2,7 +2,7 @@ const SchemaValidator = require("./SchemaValidator");
 const ApiValidator = require("./ValidationApi");
 
 const validation = async (req, res, next) => {
-  const api = new ApiValidator(process.env.EQ_SCHEMA_VALIDATOR_URL);
+  const api = new ApiValidator(process.env.EQ_VALIDATOR_URL);
   const validator = new SchemaValidator(api);
   const result = await validator.validate(res.locals.questionnaire);
   if (!result.valid) {
