e07fbe99f04466a806067b435b27d7905e3089ab
martyncolmer <martyc1974@hotmail.com>
2020-02-20 16:03:45 +0000

    Updated confirmation block type

--- src/eq_schema/block-types/Confirmation/index.js
+++ src/eq_schema/block-types/Confirmation/index.js
@@ -4,27 +4,18 @@ class Confirmation {
     this.title = "confirmation";
     this.blocks = [
       {
-        title: "You are now ready to submit this survey",
         type: "Confirmation",
         id: "confirmation",
-        description: "",
-        questions: [
-          {
-            id: "ready-to-submit-completed-question",
-            title: "Submission",
-            type: "Content",
-            guidance: {
-              content: [
-                {
-                  list: [
-                    "You will not be able to access or change your answers on submitting the questionnaire",
-                    "If you wish to review your answers please select the relevant completed sections"
-                  ]
-                }
-              ]
-            }
-          }
-        ]
+        content: {
+          title: "Submission",
+          contents: [{
+            title: "You are now ready to submit this survey",
+            list: [
+              "You will not be able to access or change your answers on submitting the questionnaire",
+              "If you wish to review your answers please select the relevant completed sections"
+            ],
+          }]
+        },
       }
     ];
   }
