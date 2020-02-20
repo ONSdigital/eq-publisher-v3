74f576bbc770ca9d0b15d6630eab457abf22dbdb
thomas.mcauliffe <tjamesmac@gmail.com>
2020-02-20 09:39:15 +0000

    Fix validation for bad request

--- src/middleware/postQuestionnaire/index.js
+++ src/middleware/postQuestionnaire/index.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
-  if (!req.body) {
+  if (
+    Object.entries(req.body).length === 0 &&
+    req.body.constructor === Object
+  ) {
     return res.sendStatus(400);
   }
   res.locals.questionnaire = req.body;
