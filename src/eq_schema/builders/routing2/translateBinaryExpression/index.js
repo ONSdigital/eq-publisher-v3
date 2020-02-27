d3be6113d9b2ea1b97792bfb6870427db33e5c72
martyncolmer <martyc1974@hotmail.com>
2020-02-27 22:57:56 +0000

    updated routing rules for option ids

--- src/eq_schema/builders/routing2/translateBinaryExpression/index.js
+++ src/eq_schema/builders/routing2/translateBinaryExpression/index.js
@@ -1,8 +1,20 @@
 const conditionConverter = require("../../../../utils/convertRoutingConditions");
+const { flatMap, filter } = require("lodash");
 
 const authorConditions = {
   UNANSWERED: "Unanswered"
 };
+
+const getOptionsFromQuestionaire = (questionnaire) => {
+  const pages = flatMap(questionnaire.sections, 'pages')
+  const answers = flatMap(pages, 'answers')
+  return flatMap(filter(answers, 'options'), 'options')
+}
+
+const getOptionValues = (optionIds, questionnaire) => {
+  const options = getOptionsFromQuestionaire(questionnaire)
+  return optionIds.map((id) => filter(options, { id })[0].label)
+}
 
 const buildAnswerBinaryExpression = ({ left, condition, right }, ctx) => {
   const returnVal = {
@@ -15,8 +27,7 @@ const buildAnswerBinaryExpression = ({ left, condition, right }, ctx) => {
   }
 
   if (right.type === "SelectedOptions") {
-    // ToDo:- add lookup for option values
-    returnVal.values = right.optionIds
+    returnVal.values = getOptionValues(right.optionIds, ctx.questionnaireJson)
   } else {
     returnVal.value = right.customValue.number;
   }
@@ -24,7 +35,7 @@ const buildAnswerBinaryExpression = ({ left, condition, right }, ctx) => {
   return returnVal;
 };
 
-const translateBinaryExpression = ( binaryExpression, ctx ) => {
+const translateBinaryExpression = (binaryExpression, ctx) => {
   if (binaryExpression.left.type === "Answer") {
     return buildAnswerBinaryExpression(binaryExpression, ctx);
   } else {
