c606b6ccd7df0e6e2d9b10ec4185c21f980bcf47
thomas.mcauliffe <tjamesmac@gmail.com>
2020-03-24 10:04:45 -0700

    Remove comments

--- src/utils/compoundFunctions/index.js
+++ src/utils/compoundFunctions/index.js
@@ -4,20 +4,14 @@ const newPipes = require("../convertPipes").newPipes;
 
 const processPipe = ctx => flow(newPipes(ctx), getInnerHTMLWithPiping);
 
-// Want to remove [contentType] from parseContent
-// Can then apply the wrapper at this level or higher
-// ---------------------------------------------------------------------------------
-
 const wrapContents = propName => content => {
-  // Catch non content
-  // ---------------------------------------------------------------------------------
-  if (!content) {
-    return;
-  }
   if (!propName || propName === "") {
     throw new Error("Property name of 'content' or 'contents' needed");
   }
-  // ---------------------------------------------------------------------------------
+
+  if (!content) {
+    return;
+  }
 
   const result = parseContent(content);
   if (result.length) {
@@ -27,21 +21,15 @@ const wrapContents = propName => content => {
   return undefined;
 };
 
-// ---------------------------------------------------------------------------------
-
-// ---------------------------------------------------------------------------------
 const reversePiping = ctx => data => {
   if (!data) {
     return "";
   }
-  // This works?
-  // ---------------------------------------------------------------------------------
+
   const content = data.contents ? data.contents : data.content;
-  // ---------------------------------------------------------------------------------
-  //   console.log(content);
+
   if (content.length) {
     content.map(items => {
-      //   const pipe = content.map(items => {
       if (items.list) {
         items.list = items.list.map(item => processPipe(ctx)(item));
       }
@@ -54,11 +42,6 @@ const reversePiping = ctx => data => {
 
   return data;
 };
-
-// ---------------------------------------------------------------------------------
-
-// At the end of this I think I want to flow the two functions together
-// ---------------------------------------------------------------------------------
 
 module.exports = {
   wrapContents,
