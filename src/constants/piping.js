af0e64a360f940303a932cce726314e3d765e01a
thomas.mcauliffe <tjamesmac@gmail.com>
2020-03-24 12:43:33 -0700

    Add piping constants

---
+++ src/constants/piping.js
@@ -0,0 +1,20 @@
+// -----------------------------------------------
+const formatPlaceholder = "format_";
+const FORMAT_CURRENCY = `${formatPlaceholder}currency`;
+const FORMAT_NUMBER = `${formatPlaceholder}number`;
+const FORMAT_DATE = `${formatPlaceholder}date`;
+
+// Transformation arguments
+// -----------------------------------------------
+const DATE_TRANSFORMATION = "date_to_format";
+const NUMBER_TRANSFORMATION = "number";
+// -----------------------------------------------
+
+module.exports = {
+  formatPlaceholder,
+  FORMAT_CURRENCY,
+  FORMAT_NUMBER,
+  FORMAT_DATE,
+  DATE_TRANSFORMATION,
+  NUMBER_TRANSFORMATION
+};
