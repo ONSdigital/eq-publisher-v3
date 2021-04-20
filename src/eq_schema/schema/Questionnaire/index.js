166f7e8047a33f52744d75bd91b5723348f3a1a0
jasehumphr <jason.humphries@ext.ons.gov.uk>
2021-04-20 09:49:50 +0100

    removing the id swap

--- src/eq_schema/schema/Questionnaire/index.js
+++ src/eq_schema/schema/Questionnaire/index.js
@@ -25,10 +25,7 @@ class Questionnaire {
     this.mime_type = "application/json/ons/eq";
     this.schema_version = "0.0.1";
     this.data_version = "0.0.3";
-    this.survey_id = this.buildSurveyId(
-      questionnaireJson.publishDetails,
-      questionnaireJson.title
-    );
+    this.survey_id = questionnaireJson.surveyId;
     this.title = questionnaireJson.title;
 
     const ctx = this.createContext(questionnaireJson);
@@ -51,14 +48,6 @@ class Questionnaire {
     };
 
     this.buildSummaryOrConfirmation(questionnaireJson.summary);
-  }
-
-  buildSurveyId(publishDetails, title) {
-    if (publishDetails) {
-      return publishDetails[0].surveyId;
-    }
-
-    return title.toLowerCase().replace(/[^a-z0-9]/g, "");
   }
 
   createContext(questionnaireJson) {
