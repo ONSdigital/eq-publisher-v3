4b357fded668d4c371530d0498ecd395a1dcd7fb
Martyn Colmer <46242834+martyncolmer@users.noreply.github.com>
2021-06-28 14:46:41 +0100

    EAR-1398 Merge all folders into group (#37)
    * Merge all foldrs into group
    
    * tidied group schema
    
    * removed test

--- src/eq_schema/schema/Group/index.js
+++ src/eq_schema/schema/Group/index.js
@@ -1,63 +1,50 @@
 const Block = require("../Block");
-const { isEmpty, reject, flatten, uniqWith, isEqual } = require("lodash");
+const { isEmpty, reject, flatten } = require("lodash");
 const {
   buildAuthorConfirmationQuestion,
 } = require("../../builders/confirmationPage/ConfirmationPage");
-const translateAuthorSkipconditions = require("../../builders/skipConditions");
 
 class Group {
-  constructor(title, folder, ctx) {
-    this.id = `group${folder.id}`;
+  constructor(title, section, ctx) {
+    this.id = `group${section.id}`;
     if (title) {
       this.title = title;
     }
-    this.blocks = this.buildBlocks(folder, ctx);
-
-    this.skip_conditions = [];
-
-    if (folder.skipConditions) {
-      this.skip_conditions.push(
-        ...translateAuthorSkipconditions(folder.skipConditions, ctx)
-      );
-    }
+    this.blocks = this.buildBlocks(section, ctx);
 
     if (!isEmpty(ctx.routingGotos)) {
       this.filterContext(this.id, ctx);
-      const skipConditions = uniqWith(
-        this.buildSkipConditions(this.id, ctx),
-        isEqual
-      );
-      this.skip_conditions.push(...skipConditions);
-    }
+      const skipConditions = this.buildSkipConditions(this.id, ctx);
 
-    if (!this.skip_conditions.length) {
-      delete this.skip_conditions;
+      if (!isEmpty(skipConditions)) {
+        this.skip_conditions = skipConditions;
+      }
     }
   }
 
   filterContext(currentId, ctx) {
     ctx.routingGotos = reject(
       ctx.routingGotos,
-      (rule) => rule.group === currentId
+      rule => rule.group === currentId
     );
   }
 
   buildSkipConditions(currentId, ctx) {
-    return reject(ctx.routingGotos, (goto) => goto.groupId === currentId).map(
+    return reject(ctx.routingGotos, goto => goto.groupId === currentId).map(
       ({ when }) => ({
-        when,
+        when
       })
     );
   }
 
-  buildBlocks(folder, ctx) {
+  buildBlocks(section, ctx) {
     return flatten(
-      folder.pages.map((page) => {
-        const block = new Block(page, folder.id, ctx);
+      section.pages.map((page) => {
+        const block = new Block(page, section.id, ctx);
         if (page.confirmation) {
           return [
             block,
-            buildAuthorConfirmationQuestion(page, folder.id, page.routing, ctx),
+            buildAuthorConfirmationQuestion(page, section.id, page.routing, ctx),
           ];
         }
         return block;
