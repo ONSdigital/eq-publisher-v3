253a8e7d5193dfbb1798f5249aa6445b604bd2c0
shelleytruong <shelleytruong@outlook.com>
2022-01-14 15:14:55 +0000

    add new legal basis option

--- src/constants/legalBases.js
+++ src/constants/legalBases.js
@@ -2,6 +2,8 @@ const contentMap = {
   NOTICE_1:
     "Notice is given under section 1 of the Statistics of Trade Act 1947.",
   NOTICE_2:
+    "Notice is given under sections 2 and 3 of the Statistics of Trade Act 1947.",
+  NOTICE_3:
     "Notice is given under sections 3 and 4 of the Statistics of Trade Act 1947.",
   VOLUNTARY: null
 };
