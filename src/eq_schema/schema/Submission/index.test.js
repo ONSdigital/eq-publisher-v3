25de5515969dcfad93606c200ff26060ad9899dd
jasehumphr <jason.humphries@ext.ons.gov.uk>
2022-03-01 14:27:31 +0000

    tests

--- src/eq_schema/schema/Submission/index.test.js
+++ src/eq_schema/schema/Submission/index.test.js
@@ -1,21 +1,23 @@
 const Submission = require(".");
 
 describe("Post Submission", () => {
-  let apiData, context;
+  let context;
 
   beforeEach(() => {
-    apiData = {
-      id: "1",
-      furtherContent: "<p>Test</p>",
-      button: "Submit",
-      title: "Submit your questionnaire",
-      warning: "You cannot view your answers after submission"
+    submission = {
+      id: "07b92a18-180c-4c35-9e94-aa6df9cdee6b",
+      furtherContent:
+        '<p>Your response will help inform decision-makers how best to support the UK population and economy at this challenging time.</p><p><a href="https://www.ons.gov.uk/surveys" target="_blank" rel="noopener noreferrer">Learn more about how we use this data</a></p>',
+      viewPrintAnswers: true,
+      emailConfirmation: true,
+      feedback: true
     };
+
     context = apiData;
   });
 
   it("should set the correct data", () => {
-    const submission = new Submission(apiData, context);
+    const submission = new Submission(submission, context);
 
     expect(submission.button).toBeTruthy();
     expect(submission.title).toBeTruthy();
