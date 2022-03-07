bd439c68b6d5a5c80bf0d9c8fdc61c91adbfb9ed
Martyn Colmer <46242834+martyncolmer@users.noreply.github.com>
2022-03-07 16:15:12 +0000

    fix wording on submission (#87)

--- src/eq_schema/schema/Submission/index.js
+++ src/eq_schema/schema/Submission/index.js
@@ -1,12 +1,14 @@
 class Submission {
   constructor(postSubmission) {
     this.button = "Submit";
+    this.title = "Submit your questionnaire";
+    this.warning = "You cannot view your answers after submission";
     if (postSubmission.viewPrintAnswers) {
       this.guidance =
         "You will have the opportunity to view, save or print a copy of your answers after submitting this survey.";
+      this.title = "Check your answers and submit";
+      this.warning = "You must submit this survey to complete it";
     }
-    this.title = "Submit your questionnaire";
-    this.warning = "You cannot view your answers after submission";
   }
 }
 
