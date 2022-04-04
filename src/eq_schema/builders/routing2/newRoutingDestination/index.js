72f95bbf55ded6c50797ae8259bf8fa403a0ee79
ashtuck <ashley.rhys.tucker@gmail.com>
2022-04-04 14:35:20 +0100

    Bug Fix

--- src/eq_schema/builders/routing2/newRoutingDestination/index.js
+++ src/eq_schema/builders/routing2/newRoutingDestination/index.js
@@ -17,9 +17,7 @@ const getOptionsFromQuestionaire = (questionnaire) => {
 const getOptionValues = (optionIds, questionnaire) => {
   const options = getOptionsFromQuestionaire(questionnaire);
 
-  const optionResults = optionIds.map(
-    (id) => filter(options, { id })[0]?.label
-  );
+  const optionResults = optionIds.map((id) => filter(options, { id })[0].label);
 
   if (optionResults === undefined || optionResults.length < 0) {
     return null;
