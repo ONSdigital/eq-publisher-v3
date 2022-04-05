93d6c74a4d3663635e828fa7f2a52592d905c88c
ashtuck <ashley.rhys.tucker@gmail.com>
2022-04-05 13:04:43 +0100

    Fixes to Or condition issue

--- src/eq_schema/builders/routing2/index.js
+++ src/eq_schema/builders/routing2/index.js
@@ -2,6 +2,9 @@ const checkValidRoutingType = require("./newRoutingDestination");
 const translateRoutingDestination = require("./translateRoutingDestination");
 const { flatMap } = require("lodash");
 const { AND, OR } = require("../../../constants/routingOperators");
+const {
+  extendingDifferentTypeKindMessage,
+} = require("graphql/validation/rules/PossibleTypeExtensions");
 
 const addRuleToContext = (goto, groupId, ctx) => {
   const destinationType = Object.keys(goto);
@@ -57,25 +60,57 @@ const buildRunnerRules = (rules, pageId, ctx, groupId) => {
 };
 
 const buildSkipConditionRules = (routing, ctx) => {
-  const skipCondition = routing.reduce((acc, route) => {
-    const { expressions, operator } = route;
+  const createSkipConditionNonOR = (routing) => {
+    return routing.reduce((acc, route) => {
+      const { expressions, operator } = route;
 
-    if (expressions.length > 1) {
-      const skipRules = expressions.map((expression) => {
-        return checkValidRoutingType(expression, ctx);
-      });
+      if (expressions.length > 1) {
+        const skipRules = expressions.map((expression) => {
+          return checkValidRoutingType(expression, ctx);
+        });
 
-      return {
-        when: { [operator.toLowerCase()]: skipRules },
-      };
-    } else {
-      const when = expressions.reduce((acc, expression) => {
-        return checkValidRoutingType(expression, ctx);
-      }, {});
+        return {
+          when: { [operator.toLowerCase()]: skipRules },
+        };
+      } else {
+        const when = expressions.reduce((acc, expression) => {
+          return checkValidRoutingType(expression, ctx);
+        }, {});
 
-      return { when: when };
-    }
-  }, {});
+        return { when: when };
+      }
+    }, {});
+  };
+
+  if (routing.length > 1) {
+    let skips = [];
+    routing.map((route) => {
+      const { expressions, operator } = route;
+
+      if (expressions.length > 1) {
+        let multiple;
+        multiple = expressions.map((expression) => {
+          return checkValidRoutingType(expression, ctx);
+        });
+
+        skips.push({ [operator.toLowerCase()]: multiple });
+
+        return skips;
+      }
+
+      skips.push(
+        expressions.reduce(
+          (acc, expression) => checkValidRoutingType(expression, ctx),
+          {}
+        )
+      );
+
+      return skips;
+    });
+    return { when: { or: skips } };
+  }
+
+  const skipCondition = createSkipConditionNonOR(routing);
 
   return skipCondition;
 };
