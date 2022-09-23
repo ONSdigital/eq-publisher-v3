d29db9840d2c14d638cd17c3b0b90dd5fbea9125
sudeep <sudeep.kunhikannan@ons.gov.uk>
2022-09-23 13:05:16 +0100

    Removed console log statement
    Signed-off-by: sudeep <sudeep.kunhikannan@ons.gov.uk>

--- src/utils/convertPipes/index.js
+++ src/utils/convertPipes/index.js
@@ -154,7 +154,7 @@ const getPipedData = (store) => (element, ctx) => {
     answerType,
     ctx
   );
-  console.log("placeholder is..." + JSON.stringify(placeholder));
+
   store.placeholders = [...store.placeholders, placeholder];
 
   return `{${removeDash(placeholderName)}}`;
