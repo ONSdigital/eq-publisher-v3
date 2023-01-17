b3c1b235848fbdcaa8e80daaded1acc15105348b
farres1 <sam.farrell@ons.gov.uk>
2023-01-17 14:51:01 +0000

    Add tests for all multiple choice answer types

--- src/utils/createAnswerCodes/index.test.js
+++ src/utils/createAnswerCodes/index.test.js
@@ -13,7 +13,7 @@ const {
   TEXTAREA,
   DATE_RANGE,
   SELECT,
-  DROPDOWN,
+  MUTUALLY_EXCLUSIVE,
 } = require("../../constants/answerTypes");
 
 describe("Create answer codes", () => {
@@ -222,125 +222,259 @@ describe("Create answer codes", () => {
     });
   });
 
-  it("should add answer codes for date range answer", () => {
-    const answer = {
-      id: "date-range-answer-1",
-      type: DATE_RANGE,
-      label: "From",
-      secondaryLabel: "To",
-      qCode: "answer-from-code",
-      secondaryQCode: "answer-to-code",
-    };
-    const questionnaire = createQuestionnaireJSON(answer);
+  describe("Date range", () => {
+    it("should add answer codes for date range answer", () => {
+      const answer = {
+        id: "date-range-answer-1",
+        type: DATE_RANGE,
+        label: "From",
+        secondaryLabel: "To",
+        qCode: "answer-from-code",
+        secondaryQCode: "answer-to-code",
+      };
+      const questionnaire = createQuestionnaireJSON(answer);
 
-    const answerCodes = createAnswerCodes(questionnaire);
+      const answerCodes = createAnswerCodes(questionnaire);
 
-    expect(answerCodes).toEqual([
-      { answer_id: "answerdate-range-answer-1from", code: "answer-from-code" },
-      { answer_id: "answerdate-range-answer-1to", code: "answer-to-code" },
-    ]);
+      expect(answerCodes).toEqual([
+        {
+          answer_id: "answerdate-range-answer-1from",
+          code: "answer-from-code",
+        },
+        { answer_id: "answerdate-range-answer-1to", code: "answer-to-code" },
+      ]);
+    });
   });
 
-  it("should add answer codes for radio answer and options", () => {
-    const answer = {
-      id: "radio-answer-1",
-      type: RADIO,
-      qCode: "answer-radio-code",
-      options: [
-        {
-          id: "radio-option-1",
-          label: "Option 1",
-        },
-        {
-          id: "radio-option-2",
-          label: "Option 2",
-        },
-        {
-          id: "radio-option-3",
-          label: "Option 3",
-          value: "Option 3 value",
-        },
-      ],
-    };
-
-    const questionnaire = createQuestionnaireJSON(answer);
-
-    const answerCodes = createAnswerCodes(questionnaire);
-
-    expect(answerCodes).toEqual([
-      {
-        answer_id: "answerradio-answer-1",
-        code: "answer-radio-code",
-      },
-      {
-        answer_id: "answerradio-answer-1",
-        answer_value: "Option 1",
-        code: "answer-radio-code",
-      },
-      {
-        answer_id: "answerradio-answer-1",
-        answer_value: "Option 2",
-        code: "answer-radio-code",
-      },
-      {
-        answer_id: "answerradio-answer-1",
-        answer_value: "Option 3 value", // sets answer_value to option.value if option.value is not undefined or null
-        code: "answer-radio-code",
-      },
-    ]);
-  });
-
-  // Radio "Other" option in Author
-  it("should add answer codes for additional radio option as an individual answer", () => {
-    const answer = {
-      id: "radio-answer-1",
-      type: RADIO,
-      qCode: "answer-radio-code",
-      options: [
-        {
-          id: "radio-option-1",
-          label: "Option 1",
-        },
-        {
-          id: "radio-option-2",
-          label: "Option 2",
-        },
-        {
-          id: "radio-option-3",
-          label: "Other answer",
-          additionalAnswer: {
-            id: "radio-additional-answer-1",
-            type: TEXTFIELD,
-            qCode: "additional-answer-code",
-            label: "Additional answer",
+  describe("Multiple choice answer types", () => {
+    it("should add answer codes for radio answer and options", () => {
+      const answer = {
+        id: "radio-answer-1",
+        type: RADIO,
+        qCode: "answer-radio-code",
+        options: [
+          {
+            id: "radio-option-1",
+            label: "Option 1",
           },
+          {
+            id: "radio-option-2",
+            label: "Option 2",
+          },
+          {
+            id: "radio-option-3",
+            label: "Option 3",
+            value: "Option 3 value",
+          },
+        ],
+      };
+
+      const questionnaire = createQuestionnaireJSON(answer);
+
+      const answerCodes = createAnswerCodes(questionnaire);
+
+      expect(answerCodes).toEqual([
+        {
+          answer_id: "answerradio-answer-1",
+          code: "answer-radio-code",
         },
-      ],
-    };
+        {
+          answer_id: "answerradio-answer-1",
+          answer_value: "Option 1",
+          code: "answer-radio-code",
+        },
+        {
+          answer_id: "answerradio-answer-1",
+          answer_value: "Option 2",
+          code: "answer-radio-code",
+        },
+        {
+          answer_id: "answerradio-answer-1",
+          answer_value: "Option 3 value", // sets answer_value to option.value if option.value is not undefined or null
+          code: "answer-radio-code",
+        },
+      ]);
+    });
 
-    const questionnaire = createQuestionnaireJSON(answer);
+    it("should add answer codes for checkbox answer and options", () => {
+      const answer = {
+        id: "checkbox-answer-1",
+        type: CHECKBOX,
+        qCode: "answer-checkbox-code",
+        options: [
+          {
+            id: "checkbox-option-1",
+            label: "Option 1",
+          },
+          {
+            id: "checkbox-option-2",
+            label: "Option 2",
+          },
+          {
+            id: "checkbox-option-3",
+            label: "Option 3",
+            value: "Option 3 value",
+          },
+        ],
+      };
 
-    const answerCodes = createAnswerCodes(questionnaire);
+      const questionnaire = createQuestionnaireJSON(answer);
 
-    expect(answerCodes).toEqual([
-      {
-        answer_id: "answerradio-answer-1",
-        code: "answer-radio-code",
-      },
-      {
-        answer_id: "answerradio-answer-1",
-        answer_value: "Option 1",
-        code: "answer-radio-code",
-      },
-      {
-        answer_id: "answerradio-answer-1",
-        answer_value: "Option 2",
-        code: "answer-radio-code",
-      },
-      {
-        answer_id: "answerradio-additional-answer-1",
-        code: "additional-answer-code",
-      },
-    ]);
+      const answerCodes = createAnswerCodes(questionnaire);
+
+      expect(answerCodes).toEqual([
+        {
+          answer_id: "answercheckbox-answer-1",
+          code: "answer-checkbox-code",
+        },
+        {
+          answer_id: "answercheckbox-answer-1",
+          answer_value: "Option 1",
+          code: "answer-checkbox-code",
+        },
+        {
+          answer_id: "answercheckbox-answer-1",
+          answer_value: "Option 2",
+          code: "answer-checkbox-code",
+        },
+        {
+          answer_id: "answercheckbox-answer-1",
+          answer_value: "Option 3 value", // sets answer_value to option.value if option.value is not undefined or null
+          code: "answer-checkbox-code",
+        },
+      ]);
+    });
+
+    it("should add answer codes for select answer and options", () => {
+      const answer = {
+        id: "select-answer-1",
+        type: SELECT,
+        qCode: "answer-select-code",
+        options: [
+          {
+            id: "select-option-1",
+            label: "Option 1",
+          },
+          {
+            id: "select-option-2",
+            label: "Option 2",
+          },
+        ],
+      };
+
+      const questionnaire = createQuestionnaireJSON(answer);
+
+      const answerCodes = createAnswerCodes(questionnaire);
+
+      expect(answerCodes).toEqual([
+        {
+          answer_id: "answerselect-answer-1",
+          code: "answer-select-code",
+        },
+        {
+          answer_id: "answerselect-answer-1",
+          answer_value: "Option 1",
+          code: "answer-select-code",
+        },
+        {
+          answer_id: "answerselect-answer-1",
+          answer_value: "Option 2",
+          code: "answer-select-code",
+        },
+      ]);
+    });
+
+    it("should add answer codes for mutually exclusive answer and options", () => {
+      const answer = {
+        id: "exclusive-answer-1",
+        type: MUTUALLY_EXCLUSIVE,
+        qCode: "answer-exclusive-code",
+        options: [
+          {
+            id: "exclusive-option-1",
+            label: "Option 1",
+          },
+          {
+            id: "exclusive-option-2",
+            label: "Option 2",
+          },
+        ],
+      };
+
+      const questionnaire = createQuestionnaireJSON(answer);
+
+      const answerCodes = createAnswerCodes(questionnaire);
+
+      expect(answerCodes).toEqual([
+        {
+          answer_id: "answerexclusive-answer-1",
+          code: "answer-exclusive-code",
+        },
+        {
+          answer_id: "answerexclusive-answer-1",
+          answer_value: "Option 1",
+          code: "answer-exclusive-code",
+        },
+        {
+          answer_id: "answerexclusive-answer-1",
+          answer_value: "Option 2",
+          code: "answer-exclusive-code",
+        },
+      ]);
+    });
+
+    // Radio "Other" option in Author
+    it("should add answer codes for additional radio option as an individual answer", () => {
+      const answer = {
+        id: "radio-answer-1",
+        type: RADIO,
+        qCode: "answer-radio-code",
+        options: [
+          {
+            id: "radio-option-1",
+            label: "Option 1",
+          },
+          {
+            id: "radio-option-2",
+            label: "Option 2",
+          },
+          {
+            id: "radio-option-3",
+            label: "Other answer",
+            additionalAnswer: {
+              id: "radio-additional-answer-1",
+              type: TEXTFIELD,
+              qCode: "additional-answer-code",
+              label: "Additional answer",
+            },
+          },
+        ],
+      };
+
+      const questionnaire = createQuestionnaireJSON(answer);
+
+      const answerCodes = createAnswerCodes(questionnaire);
+
+      expect(answerCodes).toEqual([
+        {
+          answer_id: "answerradio-answer-1",
+          code: "answer-radio-code",
+        },
+        {
+          answer_id: "answerradio-answer-1",
+          answer_value: "Option 1",
+          code: "answer-radio-code",
+        },
+        {
+          answer_id: "answerradio-answer-1",
+          answer_value: "Option 2",
+          code: "answer-radio-code",
+        },
+        {
+          answer_id: "answerradio-additional-answer-1",
+          code: "additional-answer-code",
+        },
+      ]);
+    });
   });
 });
