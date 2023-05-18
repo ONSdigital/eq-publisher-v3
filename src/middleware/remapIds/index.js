5a0d83a7569400e89879e7168b5bd7ada95c3562
farres1 <sam.farrell@ons.gov.uk>
2023-05-18 15:08:33 +0100

    Remap routing IDs to page descriptions

--- src/middleware/remapIds/index.js
+++ src/middleware/remapIds/index.js
@@ -6,6 +6,7 @@ module.exports = (req, res, next) => {
   // Object storing IDs mapped to their page descriptions - used for referencing page descriptions
   const pageDescriptionLookupTable = {};
 
+  // Adds all IDs to lookup table with page descriptions, and remaps IDs to page descriptions
   res.locals.questionnaire.sections.forEach((section) => {
     section.folders.forEach((folder) => {
       folder.pages.forEach((page) => {
@@ -22,6 +23,35 @@ module.exports = (req, res, next) => {
             );
           }
           page.id = formatPageDescription(page.pageDescription);
+        }
+      });
+    });
+  });
+
+  // Uses populated lookup table to remap IDs used in routing rules
+  res.locals.questionnaire.sections.forEach((section) => {
+    section.folders.forEach((folder) => {
+      folder.pages.forEach((page) => {
+        if (page.routing) {
+          // Remaps routing rule destination pageIds to pageDescription if the destination uses a pageId and the destination page has a page description
+          page.routing.rules.forEach((rule) => {
+            if (
+              rule.destination.pageId &&
+              pageDescriptionLookupTable[rule.destination.pageId]
+            ) {
+              rule.destination.pageId =
+                pageDescriptionLookupTable[rule.destination.pageId];
+            }
+          });
+
+          // Remaps routing `else` pageId to pageDescription if the `else` uses a pageId and the destination page has a page description
+          if (
+            page.routing.else.pageId &&
+            pageDescriptionLookupTable[page.routing.else.pageId]
+          ) {
+            page.routing.else.pageId =
+              pageDescriptionLookupTable[page.routing.else.pageId];
+          }
         }
       });
     });
