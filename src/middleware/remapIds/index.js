e09865dafdb7c2bc1be734513b5cc416bccd8218
farres1 <sam.farrell@ons.gov.uk>
2023-05-18 13:58:47 +0100

    Format page descriptions when remapped as IDs

--- src/middleware/remapIds/index.js
+++ src/middleware/remapIds/index.js
@@ -1,3 +1,7 @@
+const {
+  formatPageDescription,
+} = require("../../utils/functions/formatPageDescription");
+
 module.exports = (req, res, next) => {
   // Object storing IDs mapped to their page descriptions - used for referencing page descriptions
   const pageDescriptionLookupTable = {};
@@ -13,9 +17,11 @@ module.exports = (req, res, next) => {
               page.id
             )
           ) {
-            pageDescriptionLookupTable[page.id] = page.pageDescription;
+            pageDescriptionLookupTable[page.id] = formatPageDescription(
+              page.pageDescription
+            );
           }
-          page.id = page.pageDescription;
+          page.id = formatPageDescription(page.pageDescription);
         }
       });
     });
