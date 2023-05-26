2375d09281ec933a4475bffb63ceaab0cc1848af
sudeep <sudeep.kunhikannan@ons.gov.uk>
2023-05-26 15:48:02 +0100

    Full coverage for remap Ids
    Signed-off-by: sudeep <sudeep.kunhikannan@ons.gov.uk>

--- src/middleware/remapIds/index.test.js
+++ src/middleware/remapIds/index.test.js
@@ -66,6 +66,9 @@ describe("Remap Ids", () => {
       ],
       sections: [
         {
+          title: "Section1",
+          sectionSummary: true,
+          sectionSummaryPageDescription: "Summary page",
           folders: [
             {
               id: "0c113a37-41f8-4aed-889b-ccd4fec2f755",
@@ -74,10 +77,10 @@ describe("Remap Ids", () => {
               introductionContent: null,
               pages: [
                 {
-                  id: "dda1a11f-5e9f-4c66-bb49-00e3d84553b0",
-                  title: "<p>WE</p>",
+                  id: "page-1",
+                  title: "<p>Q1</p>",
                   pageType: "QuestionPage",
-                  description: "",
+                  pageDescription: "Question page 1",
                   descriptionEnabled: false,
                   guidance: null,
                   guidanceEnabled: false,
@@ -87,12 +90,11 @@ describe("Remap Ids", () => {
                   additionalInfoLabel: null,
                   additionalInfoContent: null,
                   additionalInfoEnabled: false,
-                  confirmation: null,
                   answers: [
                     {
                       id: "6a6e7827-eea7-4c6a-9ab1-dc1de99cccf8",
                       type: "TextField",
-                      label: "REW",
+                      label: "Label1",
                       secondaryLabel: null,
                       description: "",
                       guidance: "",
@@ -104,8 +106,122 @@ describe("Remap Ids", () => {
                       validation: null,
                     },
                   ],
-                  routing: null,
                   totalValidation: null,
+                  confirmation: {
+                    id: "confirmation-1",
+                    title: "<p>Confirmation1</p>",
+                    pageDescription: "Confirmation page",
+                    positive: {
+                      id: "positive",
+                      label: "Yes",
+                      description: "",
+                    },
+                    negative: {
+                      id: "negative",
+                      label: "No",
+                      description: "",
+                    },
+                  },
+                  routing: {
+                    id: "c3ee83a4-fb7c-42f8-ac3f-cf0b0116c648",
+                    else: {
+                      id: "page-2",
+                      pageId: "page-2",
+                    },
+                    rules: [
+                      {
+                        id: "f45b9ddd-80a4-4be6-8774-a2d3ec84028e",
+                        destination: {
+                          id: "page-3",
+                          pageId: "page-3",
+                        },
+                        expressionGroup: {
+                          id: "5a0d0b9e-891a-44b2-8d77-d4d771174c64",
+                          operator: null,
+                          expressions: [
+                            {
+                              id: "2b8366e3-2d48-4f8b-85cd-32678a5b7cdf",
+                              condition: "Equal",
+                              left: {
+                                type: "Answer",
+                                answerId:
+                                  "34974003-c483-441b-9ec4-18c978a322f0",
+                                metadataId: "",
+                              },
+                              right: {
+                                type: "Custom",
+                                customValue: {
+                                  number: 10,
+                                },
+                              },
+                            },
+                          ],
+                        },
+                      },
+                    ],
+                  },
+                },
+                {
+                  id: "page-2",
+                  title: "<p>Q2</p>",
+                  pageType: "QuestionPage",
+                  pageDescription: "Question page 2",
+                  descriptionEnabled: false,
+                  guidance: null,
+                  guidanceEnabled: false,
+                  definitionLabel: null,
+                  definitionContent: null,
+                  definitionEnabled: false,
+                  additionalInfoLabel: null,
+                  additionalInfoContent: null,
+                  additionalInfoEnabled: false,
+                  answers: [
+                    {
+                      id: "6a6e7827-eea7-4c6a-9ab1-dc1de99cccf8",
+                      type: "TextField",
+                      label: "Label2",
+                      secondaryLabel: null,
+                      description: "",
+                      guidance: "",
+                      properties: {
+                        required: false,
+                      },
+                      qCode: "",
+                      secondaryQCode: null,
+                      validation: null,
+                    },
+                  ],
+                },
+                {
+                  id: "page-3",
+                  title: "<p>Q3</p>",
+                  pageType: "QuestionPage",
+                  pageDescription: "Question page 3",
+                  descriptionEnabled: false,
+                  guidance: null,
+                  guidanceEnabled: false,
+                  definitionLabel: null,
+                  definitionContent: null,
+                  definitionEnabled: false,
+                  additionalInfoLabel: null,
+                  additionalInfoContent: null,
+                  additionalInfoEnabled: false,
+                  answers: [
+                    {
+                      id: "6a6e7827-eea7-4c6a-9ab1-dc1de99cccf8",
+                      type: "TextField",
+                      label: "Label3",
+                      secondaryLabel: null,
+                      description: "",
+                      guidance: "",
+                      properties: {
+                        required: false,
+                      },
+                      qCode: "",
+                      secondaryQCode: null,
+                      validation: null,
+                    },
+                  ],
                 },
               ],
             },
@@ -123,7 +239,66 @@ describe("Remap Ids", () => {
     remapIds(req, res, next);
   });
 
-  it("should pass data through", () => {
-    expect(next).toHaveBeenCalledWith();
+  it("should move to next if successful", () => {
+    req.body = questionnaire;
+
+    middleware = remapIds(req, res, next);
+
+    expect(res.locals.questionnaire).toEqual(questionnaire);
+    expect(next).toHaveBeenCalled();
+  });
+
+  it("should remap section page id", () => {
+    req.body = questionnaire;
+
+    middleware = remapIds(req, res, next);
+
+    expect(res.locals.questionnaire).toEqual(questionnaire);
+    expect(res.locals.questionnaire.sections[0].id).toEqual("summary-page");
+  });
+
+  it("should remap question page id", () => {
+    req.body = questionnaire;
+
+    middleware = remapIds(req, res, next);
+
+    expect(res.locals.questionnaire).toEqual(questionnaire);
+    expect(res.locals.questionnaire.sections[0].folders[0].pages[0].id).toEqual(
+      "question-page-1"
+    );
+  });
+
+  it("should remap confirmation question page id", () => {
+    req.body = questionnaire;
+
+    middleware = remapIds(req, res, next);
+
+    expect(res.locals.questionnaire).toEqual(questionnaire);
+    expect(
+      res.locals.questionnaire.sections[0].folders[0].pages[0].confirmation.id
+    ).toEqual("confirmation-page");
+  });
+
+  it("should remap destination page id", () => {
+    req.body = questionnaire;
+
+    middleware = remapIds(req, res, next);
+
+    expect(res.locals.questionnaire).toEqual(questionnaire);
+    expect(
+      res.locals.questionnaire.sections[0].folders[0].pages[0].routing.rules[0]
+        .destination.id
+    ).toEqual("question-page-3");
+  });
+
+  it("should remap routing else page id", () => {
+    req.body = questionnaire;
+
+    middleware = remapIds(req, res, next);
+
+    expect(res.locals.questionnaire).toEqual(questionnaire);
+    expect(
+      res.locals.questionnaire.sections[0].folders[0].pages[0].routing.else.id
+    ).toEqual("question-page-2");
   });
 });
