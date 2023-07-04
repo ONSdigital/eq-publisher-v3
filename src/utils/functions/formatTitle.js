7a4cf460ea661feafbff6b5a9428b532000de696
Jason Lavarinhas <jason.da.cunha.lavarinhas@ons.gov.uk>
2023-07-04 11:17:30 +0100

    Temp change

--- src/utils/functions/formatTitle.js
+++ src/utils/functions/formatTitle.js
@@ -1,5 +1,10 @@
+const { replace } = require("lodash/fp");
+
 const formatTitle = (stringToFormat) => {
-  return stringToFormat ? stringToFormat.replace(/['’]/g, "&#39;") : "";
+  //console.log(typeof stringToFormat === "string");
+  //return stringToFormat ? stringToFormat.replace(/&apos;/g, "&#39;") : "";
+  const formatted = replace(/&apos;/g, `&#39;`, stringToFormat);
+  return formatted;
 };
 
 module.exports = { formatTitle };
