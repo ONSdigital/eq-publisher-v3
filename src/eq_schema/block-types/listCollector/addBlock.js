629dff5c277d74e3028cbdd2d3f61132b09029b8
shelleytruong <shelleytruong@outlook.com>
2023-09-21 16:34:11 +0100

    additional content LC page

--- src/eq_schema/block-types/listCollector/addBlock.js
+++ src/eq_schema/block-types/listCollector/addBlock.js
@@ -7,23 +7,52 @@ const { getList } = require("../../../utils/functions/listGetters");
 const {
   formatPageDescription,
 } = require("../../../utils/functions/formatPageDescription");
+const {
+  wrapContents,
+  reversePipeContent,
+} = require("../../../utils/compoundFunctions");
 
 const processPipe = (ctx) => flow(convertPipes(ctx), getInnerHTMLWithPiping);
-
+const reversePipe = (ctx) =>
+  flow(wrapContents("contents"), reversePipeContent(ctx));
 class AddBlock {
   constructor(page, ctx) {
     this.id = `add-block-${formatPageDescription(page.pageDescription)}`;
     this.type = "ListAddQuestion";
     this.page_title = processPipe(ctx)(page.pageDescription);
 
-    this.cancel_text = "Don’t need to add this item";
+    this.cancel_text = "Don’t nCDeed to add this item";
+    this.question = this.buildQuestion(page, ctx);
+  }
+
+  buildQuestion(page, ctx) {
     const listAnswers = getList(ctx, page.listId).answers;
-    this.question = {
+    const question = {
       id: `add-block-question-${formatPageDescription(page.pageDescription)}`,
-      type: "General",
-      title: processPipe(ctx)(page.title),
-      answers: this.buildAnswers(listAnswers, ctx),
     };
+    if (page.descriptionEnabled && page.description) {
+      question.description = [convertPipes(ctx)(page.description)];
+    }
+
+    if (page.guidanceEnabled && page.guidance) {
+      question.guidance = reversePipe(ctx)(page.guidance);
+    }
+
+    if (
+      page.definitionEnabled &&
+      (page.definitionLabel || page.definitionContent)
+    ) {
+      question.definitions = [
+        {
+          title: processPipe(ctx)(page.definitionLabel),
+          ...reversePipe(ctx)(page.definitionContent),
+        },
+      ];
+    }
+    question.type = "General";
+    question.title = processPipe(ctx)(page.title);
+    question.answers = this.buildAnswers(listAnswers, ctx);
+    return question;
   }
 
   buildAnswers(answers, ctx) {
--- src/eq_schema/block-types/listCollector/editBlock.js
+++ src/eq_schema/block-types/listCollector/editBlock.js
@@ -7,7 +7,13 @@ const { getList } = require("../../../utils/functions/listGetters");
 const {
   formatPageDescription,
 } = require("../../../utils/functions/formatPageDescription");
+const {
+  wrapContents,
+  reversePipeContent,
+} = require("../../../utils/compoundFunctions");
 
+const reversePipe = (ctx) =>
+  flow(wrapContents("contents"), reversePipeContent(ctx));
 const processPipe = (ctx) => flow(convertPipes(ctx), getInnerHTMLWithPiping);
 
 class EditBlock {
@@ -16,13 +22,37 @@ class EditBlock {
     this.type = "ListEditQuestion";
     this.page_title = processPipe(ctx)(page.pageDescription);
     this.cancel_text = "Don’t need to edit this item";
+    this.question = this.buildQuestion(page, ctx);
+  }
+
+  buildQuestion(page, ctx) {
     const listAnswers = getList(ctx, page.listId).answers;
-    this.question = {
+    const question = {
       id: `edit-block-question-${formatPageDescription(page.pageDescription)}`,
-      type: "General",
-      title: processPipe(ctx)(page.title),
-      answers: this.buildAnswers(listAnswers, ctx),
     };
+    if (page.descriptionEnabled && page.description) {
+      question.description = [convertPipes(ctx)(page.description)];
+    }
+
+    if (page.guidanceEnabled && page.guidance) {
+      question.guidance = reversePipe(ctx)(page.guidance);
+    }
+
+    if (
+      page.definitionEnabled &&
+      (page.definitionLabel || page.definitionContent)
+    ) {
+      question.definitions = [
+        {
+          title: processPipe(ctx)(page.definitionLabel),
+          ...reversePipe(ctx)(page.definitionContent),
+        },
+      ];
+    }
+    question.type = "General";
+    question.title = processPipe(ctx)(page.title);
+    question.answers = this.buildAnswers(listAnswers, ctx);
+    return question;
   }
 
   buildAnswers(answers, ctx) {
