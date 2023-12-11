2f432a870d18caf16b278e41bec903e22d16be9e
Martyn Colmer <46242834+martyncolmer@users.noreply.github.com>
2023-12-11 17:11:03 +0000

    fix delimeter (#199)

--- src/utils/convertPipes/PlaceholderObjectBuilder.js
+++ src/utils/convertPipes/PlaceholderObjectBuilder.js
@@ -109,7 +109,7 @@ const placeholderObjectBuilder = (
     }
     if (["array"].includes(AnswerType)) {
       argumentList = {
-        delimiter: ", ",
+        delimiter: ",&nbsp;",
       };
       valueSource = [valueSource];
     }
