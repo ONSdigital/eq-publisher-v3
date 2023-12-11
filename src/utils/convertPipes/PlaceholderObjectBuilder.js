529cea40eb253ab0cb08898a3f23bcd33b25eb60
martyncolmer <martyc1974@hotmail.com>
2023-12-11 16:58:23 +0000

    added space to delimiter

--- src/utils/convertPipes/PlaceholderObjectBuilder.js
+++ src/utils/convertPipes/PlaceholderObjectBuilder.js
@@ -109,7 +109,7 @@ const placeholderObjectBuilder = (
     }
     if (["array"].includes(AnswerType)) {
       argumentList = {
-        delimiter: ",",
+        delimiter: ", ",
       };
       valueSource = [valueSource];
     }
