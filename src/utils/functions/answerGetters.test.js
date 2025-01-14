1c446c9289a152bcd7064feba2ddede4ce04d0ea
farres1 <sam.farrell@ons.gov.uk>
2025-01-14 11:31:54 +0000

    Add test for pages without answers

--- src/utils/functions/answerGetters.test.js
+++ src/utils/functions/answerGetters.test.js
@@ -34,4 +34,40 @@ describe("getAnswerById", () => {
       label: "Answer 2",
     });
   });
+
+  it("should return an answer by id when questionnaire contains pages without answers", () => {
+    const ctx = {
+      questionnaireJson: {
+        sections: [
+          {
+            id: "section-1",
+            folders: [
+              {
+                id: "folder-1",
+                pages: [
+                  {
+                    id: "page-1",
+                    answers: [
+                      {
+                        id: "answer-1",
+                        label: "Answer 1",
+                      },
+                    ],
+                  },
+                  {
+                    id: "page-2",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    expect(getAnswerById(ctx, "answer-1")).toMatchObject({
+      id: "answer-1",
+      label: "Answer 1",
+    });
+  });
 });
