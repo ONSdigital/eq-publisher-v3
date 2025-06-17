d2571dbce8c7bd88b484f823eb3bd34ed52f8b63
Martyn Colmer <46242834+martyncolmer@users.noreply.github.com>
2025-06-17 13:44:42 +0100

    fix for piping supp data in answer label (#227)
    * fix for piping supp in answer label
    
    Signed-off-by: Martyn <46242834+martyncolmer@users.noreply.github.com>
    
    * formatting
    
    Signed-off-by: Martyn <46242834+martyncolmer@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Martyn <46242834+martyncolmer@users.noreply.github.com>

--- src/utils/functions/sectionGetters.js
+++ src/utils/functions/sectionGetters.js
@@ -14,4 +14,25 @@ const getSectionByPageId = (ctx, pageId) => {
   return result;
 };
 
-module.exports = { getSectionByPageId };
+const getSectionByAnswerId = (ctx, answerId) => {
+  let result;
+  if (ctx && ctx.questionnaireJson && ctx.questionnaireJson.sections) {
+    ctx.questionnaireJson.sections.forEach((section) => {
+      section.folders.forEach((folder) => {
+        folder.pages.forEach((page) => {
+          if (page.answers) {
+            page.answers.forEach((answer) => {
+              if (answer.id === answerId) {
+                result = section;
+              }
+            });
+          }
+        });
+      });
+    });
+  }
+  return result;
+};
+
+
+module.exports = { getSectionByPageId, getSectionByAnswerId };
